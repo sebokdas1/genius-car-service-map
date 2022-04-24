b2a0a89ce690ce6cd366bf53d9ea78ddd14afcc3
sebokdas1 <sebok.das.ff@gmail.com>
2022-04-24 21:15:55 +0600

    form and user info added

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetail from '../../../hooks/useServiceDetail';
 
 const Checkout = () => {
     const { serviceId } = useParams();
     const [service] = useServiceDetail(serviceId);
+    const [user, setUser] = useState({
+        name: 'sebok',
+        email: 'sebok@def.co',
+        address: "Cox'sbazar, Bangladesh",
+        phone: '01730661569'
+    });
+    const handleAddressChange = e => {
+        const { address, ...rest } = user;
+        const newAddress = e.target.value;
+        const newUser = { address: newAddress, ...rest };
+        console.log(newUser);
+        setUser(newUser);
+    }
     return (
         <div className='w-50 mx-auto'>
             <h2>Please Order: {service.name}</h2>
             <form>
-                <input type="text" name='name' placeholder='Name' required />
+                <input className='w-100 mb-2' value={user.name} type="text" name='name' placeholder='name' required />
+                <br />
+                <input className='w-100 mb-2' value={user.email} type="email" name='email' placeholder='email' required />
+                <br />
+                <input className='w-100 mb-2' type="text" value={service.name} name='service' placeholder='service' required />
+                <br />
+                <input className='w-100 mb-2' value={user.address} onChange={handleAddressChange} type="text" name='address' placeholder='address' required />
+                <br />
+                <input className='w-100 mb-2' value={user.phone} type="text" name='phone' placeholder='phone' required />
+                <br />
+                <input className='btn btn-primary' type="submit" value="Place Order" />
             </form>
         </div>
     );
